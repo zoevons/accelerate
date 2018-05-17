26630377b9855b221201799eb9e8e49ec2e2f04a
zoevons <zoevons@gmail.com>
2018-05-17 08:58:09 +0200

    putting folder onto Github for the first time

---
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The template for displaying the archive case studies page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary-2" class="site-content">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post();
+					$image_1 = get_field('image_1');
+					$size = "full";
+					$services = get_field('services'); ?>
+
+		  	<article class="case-study">
+				  <aside class="case-study-sidebar">
+						<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+						<h4 class="archive-header"><?php echo $services; ?></h4>
+						<?php the_excerpt(); ?>
+						<p class="archive-paragraph"><strong><a href="<?php the_permalink(); ?>" >View Project</a></strong></p>
+			    </aside>
+
+			 		<div class="case-study-images">
+			  	  <a href="<?php the_permalink(); ?>" >
+							<?php if ($image_1) {
+								echo wp_get_attachment_image( $image_1, $size );
+							} ?>
+			     </a>
+			   </div>
+   	  </article>
+
+			<?php endwhile; // end of the loop. ?>
+			</div><!-- .main-content -->
+  </div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * The template for displaying the case study page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div class="main-content" role="main">
+
+			<?php while ( have_posts() ) : the_post();
+			  $size = "full";
+				$services = get_field('services');
+				$client = get_field('client');
+				$link = get_field('site-link');
+				$image_1 = get_field('image_1');
+				$image_2 = get_field('image_2');
+				$image_3 = get_field('image_3'); ?>
+
+  	<article class="case-study">
+		  <aside class="case-study-sidebar">
+			  <h2><?php the_title(); ?></h2>
+			  <h4><?php echo $services; ?></h4>
+			  <h5>Client: <?php echo $client; ?></h5>
+
+		    <?php> the_content(); ?>
+
+		     <p><strong><a href="<?php echo $link; ?>" >Site Link</a></strong></p>
+		 </aside>
+
+		<div class="case-study-images">
+			<?php if ($image_1) {
+				echo wp_get_attachment_image( $image_1, $size );
+			} ?>
+
+			<?php if ($image_2) {
+				echo wp_get_attachment_image( $image_2, $size );
+			} ?>
+
+			<?php if ($image_3) {
+				echo wp_get_attachment_image( $image_3, $size );
+			} ?>
+		</div>
+
+      </article>
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+
+
+	</div><!-- #primary -->
+
+	<nav id="navigation" class="container">
+		<div class="left"><a href="<?php echo site_url('/blog/') ?>">&larr; <span>Back to Work</span></a></div>
+	</nav>
+
+<?php get_footer(); ?>
